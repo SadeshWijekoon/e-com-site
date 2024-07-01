0ec7f76797dfdded295108dea373d873c08a9d26
SadeshWijekoon <10898706@students.plymouth.ac.uk>
2024-07-01 14:15:10 +0530

    set up header

--- src/Componts/Header/Header.jsx
+++ src/Componts/Header/Header.jsx
@@ -6,18 +6,26 @@ import cart from '../../img/cart.svg';
 
 const Header = () => {
   return (
-    <header>
+    <header className='w-full p-5 bg-my-background flex items-center justify-between'>
       {/*header left*/}
-      <div>
-        <img src={menu} alt='menu' />
-        <h1>Sadesh<span>Wijekoon</span></h1>
-        <div>
-          <input type='text' placeholder='Search'/>
-          <img src={search} alt='search'/>
+      <div className='flex items-center'>
+        <img src={menu} 
+          alt='menu'
+          className='w-6 h-6 object-contain cursor-pointer' />
+        <h1 className='ml-2 text-sm font-bold'>Sadesh<span className='text-rose-500'>Wijekoon</span></h1>
+
+        <div className='overflow-hidden ml-2 flex items-center rounded-full bg-[#eee0e0]' > {/* bg-[#e0c3c3]*/}
+          <input type='text' placeholder='Search' className='outline-none p-2 font-semibold text-sm w-[200px] bg-inherit '/> {/*bg-inherit*/}
+          <img src={search} 
+            alt='search'
+            className='w-6 h-6 object-contain cursor-pointer'/>
         </div>
-      </div>
+
+        </div>
       {/*header right*/}
-      <img src={cart} alt='cart'/>
+         <img src={cart} 
+           alt='cart'
+           className='w-6 h-6 object-contain cursor-pointer'/>
     </header>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'my-background': '#d99e9e',
+      },
+    },
   },
   plugins: [],
 }

37f5d779de35e0ee180b3b2e1b6e4573a9cb1ef2
SadeshWijekoon <10898706@students.plymouth.ac.uk>
2024-07-01 22:08:49 +0530

    add material ui icon

--- src/Componts/Header/Header.jsx
+++ src/Componts/Header/Header.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import menu from '../../img/menu.svg';
-import search from '../../img/search.svg';
-import cart from '../../img/cart.svg';
+
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 
 const Header = () => {
@@ -9,26 +9,24 @@ const Header = () => {
     <header className='fixed top-0 left-0 z-[100] drop-shadow-header-shadow w-full p-5 bg-my-background flex items-center justify-between'>
       {/*header left*/}
       <div className='flex items-center'>
-        <img src={menu} 
-          alt='menu'
-          className='w-6 h-6 object-contain cursor-pointer' />
-        <h1 className='ml-2 text-sm font-bold'>Sadesh<span className='text-rose-500'>Wijekoon</span></h1>
+        <MenuIcon sx={
+          {color:'#800909'}
+        }/>
+        <h1 className='ml-2 text-sm font-bold'>Sadesh<span className='text-[#800909]'>Wijekoon</span></h1>
 
         <div className='overflow-hidden ml-2 flex items-center rounded-full bg-[#f8dddd]' > {/* bg-[#e0c3c3]*/}
           <input 
            type='text' 
            placeholder='Search' 
            className='ml-1 outline-none p-2 font-semibold text-sm w-[200px] bg-inherit '/> 
-          <img src={search} 
-            alt='search'
-            className='mr-2 w-6 h-6 object-contain cursor-pointer'/>
+          <SearchIcon sx={
+          {color:'#800909'}
+        }/>
         </div>
 
         </div>
       {/*header right*/}
-         <img src={cart} 
-           alt='cart'
-           className='w-6 h-6 object-contain cursor-pointer'/>
+         <ShoppingCartIcon className='text-[#800909]'/> {/*tailwind type*/}
     </header>
   )
 }

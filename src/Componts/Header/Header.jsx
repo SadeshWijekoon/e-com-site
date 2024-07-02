44814382922ad6c4b1d448f2bad3bb3cc4d80927
SadeshWijekoon <10898706@students.plymouth.ac.uk>
2024-07-02 23:58:16 +0530

    Update Header.jsx

--- src/Componts/Header/Header.jsx
+++ src/Componts/Header/Header.jsx
@@ -2,9 +2,13 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { useState } from 'react';
 
 
 const Header = () => {
+
+ const [isSearch,setIsSearch]=useState(false)
+
   return (
     <header className='fixed top-0 left-0 z-[100] drop-shadow-header-shadow w-full
      py-5 px-2 bg-my-background flex items-center justify-between'>
@@ -24,9 +28,14 @@ const Header = () => {
           <input 
            type='text' 
            placeholder='Search' 
-           className='hidden  sm:inline-block ml-1 outline-none p-2 font-semibold text-sm w-[200px] bg-inherit '/>
-          <IconButton sx={{color:'red'}}>
-          <SearchIcon sx={{
+           style={{
+            display: window.innerWidth<640? isSearch ? 'inline-block': 'none':
+            'inline-block'
+           }}
+           className='hidden  sm:inline-block ml-1 outline-none p-2 
+           font-semibold text-sm w-[200px] bg-inherit '/>
+          <IconButton onClick={()=>setIsSearch(true)} sx={{color:'red'}}>
+          <SearchIcon  sx={{
         color: '#4c4f4d',
         '&:hover': {
           color: '#800909',

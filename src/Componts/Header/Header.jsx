4e2338945f12601352bd07465579d75e21c8769e
SadeshWijekoon <10898706@students.plymouth.ac.uk>
2024-07-20 19:44:53 +0530

    add the cooments

--- src/Componts/Header/Header.jsx
+++ src/Componts/Header/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
      p-2 px-2 bg-my-background flex items-center justify-between'>
       {/*header left*/}
       <div className='flex items-center'>
-        <IconButton sx={{color:'red'}}>
+        <IconButton >  {/* wrap the menuicon by using IconButton*/}
         <MenuIcon sx={{
         color: '#4c4f4d',
         '&:hover': {
@@ -23,20 +23,20 @@ const Header = () => {
         },
       }} />
         </IconButton>
-        <h1 style={{
+        <h1 style={{  
             display: window.innerWidth<640? 
              isSearch ?  
-               ' none':  
-               ' inline-block':
-               'inline-block'
-           }} className='ml-2 text-sm font-bold'>
+               ' none':  /*apply when issearch is true */
+               ' inline-block': /*apply when issearch is false */
+               'inline-block' /* applying only when the inner widthmorethan 640*/  }}        
+            className='ml-2 text-sm font-semibold'> {/* this is tailwind css and its apply whether inline css apply or not*/}
           Sadesh <span className='text-custom-red'>Wijekoon</span>
         </h1>
 
         <div style={{
             backgroundColor: window.innerWidth<640? 
              isSearch ?  
-               ' #f8dddd':  
+               ' #f8dddd':  /* inputbox bg coler applying by using some logic same as above*/
                ' inherit':
                '#f8dddd'
            }} className='overflow-hidden ml-2 flex items-center rounded-full bg-[#f8dddd]' > {/* bg-[#e0c3c3]*/}
@@ -52,12 +52,12 @@ const Header = () => {
                transition:'all 400ms ease-in-out'
            }}
            className='   hidden  sm:inline-block ml-1 outline-none p-2 
-           font-semibold text-sm w-[200px] bg-inherit '/>
-          <IconButton onClick={()=>{
-            if(window.innerWidth<640){
-              setIsSearch(!isSearch)
-            }
-          }} sx={{color:'red'}}>
+           font-semibold text-sm w-[200px] bg-inherit ' /> {/*apply this whter inlinecss apply or not */}
+          <IconButton onClick={()=>{  
+            if(window.innerWidth<640){ 
+              setIsSearch(!isSearch)     
+            }}} /*wrap the searchicon and got the onclick event */
+              >  
           <SearchIcon  sx={{
         color: '#4c4f4d',
         '&:hover': {
@@ -69,7 +69,7 @@ const Header = () => {
 
         </div>
       {/*header right*/}
-       <IconButton  sx={{color:'red'}}>
+       <IconButton  >
           <div className=' relative  p-1'>
           <ShoppingCartIcon className='text-[#4c4f4d] hover:text-[#800909]'/>
           <div className=' absolute top-[2px] right-0 text-white  text-[7px] font bg-[#800909] flex items-center justify-center w-3 h-3 rounded-full   '>2</div>
